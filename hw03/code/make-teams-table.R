d4765456470b7b8b0aed474d86fff9bc7a5faeff
d71chang <d7cheng@berkeley.edu>
2017-10-06 22:29:49 -0700

    adding data

---
+++ hw03/code/make-teams-table.R
@@ -0,0 +1,5 @@
+# Title: NBA 2017 Teams 
+# Description: This script contains the commands for analyzing the structure
+# NBA 2017 teams. The functions will produce a comprehensive summary of the data.
+# Input: nba2017-teams.csv
+# Output: Data Table 
